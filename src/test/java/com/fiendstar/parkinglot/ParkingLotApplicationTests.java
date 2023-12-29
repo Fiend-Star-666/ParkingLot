2da10fac155a30286bc1edc3fc6f6bf040bd990a
Gursimran <gurmsi1060@outlook.com>
2023-12-29 23:17:19 +0530

    Initial Commit

---
+++ src/test/java/com/fiendstar/parkinglot/ParkingLotApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fiendstar.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ParkingLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    //@MockBean
+
+}
